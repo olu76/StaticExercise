05101bf01fcd26963a6a2288b5cb95d6704f0699
olumide azeez afolabi <midas44000@yahoo.com>
2021-11-10 01:52:17 -0600

    initial commit

---
+++ StaticExercise Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(40);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(60);
+
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise Console/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise_Console
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            return (Fahrenheit - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius / 1.8) + 32;
+        }
+    }
+}
+
